52d6ad734293e0869c45d29722e1254963c4bbea
Sai Swarup Noone <32135087+nooneswarup@users.noreply.github.com>
2023-02-03 03:31:10 +0000

    about page

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+      <div className="container">
+        <Head>
+          <title>about</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main>
+        <h1>hello</h1>
+        </main>
+        </div>
+        )}
